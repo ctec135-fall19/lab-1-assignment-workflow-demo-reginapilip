0a03ca378a3a06405d3036c4e29bfc17098a6469
Regina Pilipchuk <regina.pilipchuk@gmail.com>
2019-10-01 14:27:34 -0700

    First program - completed assignment

---
+++ MyFirstSolution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
